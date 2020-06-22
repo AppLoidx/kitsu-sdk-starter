71c13fcabe0387daf27f6347ba7c1f0862bcd532
Arthur Kupriyanov <apploidyakutsk@gmail.com>
2020-06-22 21:35:33 +0300

    for ApplicationContext

---
+++ src/main/java/com/apploidx/kitsusdkstarter/Application.java
@@ -0,0 +1,10 @@
+package com.apploidx.kitsusdkstarter;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Arthur Kupriyanov on 22.06.2020
+ */
+@SpringBootApplication
+public class Application {
+}
