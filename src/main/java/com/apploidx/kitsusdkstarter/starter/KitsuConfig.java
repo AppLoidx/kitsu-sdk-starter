dd722d8e9e32bd6464ae0f00867fe7b3ddba7e8a
Arthur Kupriyanov <apploidyakutsk@gmail.com>
2020-06-22 21:38:04 +0300

    delete bean autowiring

--- src/main/java/com/apploidx/kitsusdkstarter/starter/KitsuConfig.java
+++ src/main/java/com/apploidx/kitsusdkstarter/starter/KitsuConfig.java
@@ -1,7 +1,6 @@
 package com.apploidx.kitsusdkstarter.starter;
 
 import com.apploidx.kitsusdkstarter.client.KitsuClient;
-import com.apploidx.kitsusdkstarter.http.KitsuHttpTransport;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -13,11 +12,5 @@ import org.springframework.context.annotation.Configuration;
 @ConditionalOnClass({KitsuClient.class})
 @ComponentScan(basePackages = "com.apploidx.kitsusdkstarter")
 public class KitsuConfig {
-    private final KitsuClient kitsuClient;
-    private final KitsuHttpTransport kitsuHttpTransport;
 
-    public KitsuConfig(KitsuClient kitsuClient, KitsuHttpTransport kitsuHttpTransport) {
-        this.kitsuClient = kitsuClient;
-        this.kitsuHttpTransport = kitsuHttpTransport;
-    }
 }
